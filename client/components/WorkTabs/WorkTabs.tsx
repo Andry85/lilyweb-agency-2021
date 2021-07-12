761433480c0125d614ac4fd3270b68310116eb16
Andry85 <andrii.shapoval@smile-ukraine.com>
2021-07-12 18:06:57 +0300

    added masonry

--- client/components/WorkTabs/WorkTabs.tsx
+++ client/components/WorkTabs/WorkTabs.tsx
@@ -1,8 +1,10 @@
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 import styles from './WorkTabs.module.scss'
 import {useSelector, useDispatch} from 'react-redux'; 
 import {changeCategoryID} from "../../redux/actions/actions"; 
 import type {RootState} from '../../redux/store';
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 type WorkTabsProp = {
@@ -40,21 +42,32 @@ function WorkTabs({categories}: WorkTabsProp): JSX.Element {
         <div className={styles.workTabsContainer}>
             {categories.filter(item => item.id === tabActive).map(filteredData => (
               <div key={filteredData.id} className={styles.workTabsContainer__inner}>
-                
-
-
-                {filteredData.works.map((item) => 
-                    <div key={item.id} className={styles.workTabsContainer__item}>     
-                        <div key={item.id} className={styles.workTabsContainer__picture}> 
-                          <Image
-                              src={item.pic}
-                              alt=""
-                              layout='fill'
-                          />
-                        </div>
-                    </div>
-                )}
-
+                <ResponsiveMasonry
+                    columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+                >
+                    <Masonry gutter="20px" columnsCount={3}>
+                      {filteredData.works.map((item) => 
+                          <div key={item.id} className={styles.workTabsContainer__item}>     
+                              <div className={styles.workTabsContainer__picture}> 
+                                <Image
+                                    src={item.pic}
+                                    alt=""
+                                    layout='fill'
+                                />
+                              </div>
+                              <div className={styles.workTabsContainer__content}>
+                                <div className={styles.workTabsContainer__body}>
+                                  <div className={styles.workTabsContainer__link}>
+                                    <Link href={item.innerLink}>
+                                      <a className={styles.workTabsContainer__linkPic}></a>
+                                    </Link>
+                                  </div>
+                                </div>
+                              </div>
+                          </div>
+                      )}
+                    </Masonry>
+                </ResponsiveMasonry>
               </div>
             ))}
         </div>
