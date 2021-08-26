6a57d85832dc4314068db9b9a29f5f184967cf1d
ashapoval <shapovala@ukr.net>
2021-08-26 21:05:46 +0300

    added page details

--- client/pages/works/[id].js
+++ client/pages/works/[id].js
@@ -51,26 +51,52 @@ export async function getStaticProps({ params }) {
     }
   }
 
-  const resultList = jsonObj.worksPage.categories;
-  const innerLink = resultList.map(function (element) {
+  const categories = jsonObj.worksPage.categories;
+  const worksinCategory = categories.map(function (element) {
     return element.works;
   });
 
-  const flatLinks = innerLink.flat();
-  const works = flatLinks.find(function (element) {
+  const flatWorksinCategory = worksinCategory.flat();
+  const resultWork = flatWorksinCategory.find(function (element) {
     return element.id == params.id;
   });
+  const nextWork = flatWorksinCategory.find(function (element) {
+
+
+    
+    if (params.id == flatWorksinCategory.length - 1) {
+      return element.id == 0;
+    } else {
+      return element.id == parseInt(params.id) + 1;
+    }
+
+  });
+  const prevWork = flatWorksinCategory.find(function (element) {
+
+    if (params.id > 0) {
+      return element.id == parseInt(params.id) - 1;
+    } else {
+      return element.id == flatWorksinCategory.length -1;
+    }
+  });
+
+
+
+
+
 
   // Pass post data to the page via props
   return { 
     props: { 
-      works,
-      jsonObj
+      resultWork,
+      jsonObj,
+      nextWork,
+      prevWork
     } 
   }
 }
 
-export default function Work({works,jsonObj}) {
+export default function Work({resultWork,jsonObj, nextWork, prevWork}) {
 
   return (
     <Layout>
@@ -84,76 +110,76 @@ export default function Work({works,jsonObj}) {
           </div>  
         </div>
         <div className={styles.workWrapper}>
-            <h3 className={styles.workWrapper__cat}>{works.cat}</h3>
-            <h2 className={styles.workWrapper__title}>{works.title}</h2>
+            <h3 className={styles.workWrapper__cat}>{resultWork.cat}</h3>
+            <h2 className={styles.workWrapper__title}>{resultWork.title}</h2>
             <div className={styles.workPreambule}>
               <div className={styles.workPreambule__left}>
-                <p className={styles.workPreambule__smallDesc}>{works.smallDesc}</p>
-                <p className={styles.workPreambule__longDesc}>{works.longDesc}</p>
+                <p className={styles.workPreambule__smallDesc}>{resultWork.smallDesc}</p>
+                <p className={styles.workPreambule__longDesc}>{resultWork.longDesc}</p>
               </div>
               <div className={styles.workPreambule__right}>
                 <aside>
                   <ul>
                     <li>
                       <h4>Client</h4>
-                      <p>{works.client}</p>
+                      <p>{resultWork.client}</p>
                     </li>
                     <li>
                       <h4>Technology</h4>
-                      <p>{works.technology}</p>
+                      <p>{resultWork.technology}</p>
                     </li>
                     <li>
                       <h4>Created Date</h4>
-                      <p>{works.createdDate}</p>
+                      <p>{resultWork.createdDate}</p>
                     </li>
                     <li>
                       <h4>Website</h4>
-                      <p>{works.link}</p>
+                      <p>{resultWork.link}</p>
                     </li>
                   </ul>
                 </aside>
               </div>
             </div>
             <ul className={styles.workImages}>
-              {works.images.map((item, index) => (
+              {resultWork.images.map((item, index) => (
                   <li key={index}>
                     <img src={item} />
                   </li>
               ))}
             </ul>
             <div className={styles.workPostambule}>
-                <h2 className={styles.workPostambule__title}>{works.prew.title}</h2>
-                <p className={styles.workPostambule__desc}>{works.prew.desc}</p>
+                <h2 className={styles.workPostambule__title}>{resultWork.prew.title}</h2>
+                <p className={styles.workPostambule__desc}>{resultWork.prew.desc}</p>
                 <div className={styles.workPostambule__content}>
                   <div className={styles.workPostambule__mobile}>
-                    <img src={works.prew.mobilePic} />
+                    <img src={resultWork.prew.mobilePic} />
                   </div>
                   <div className={styles.workPostambule__desktop}>
-                  <img src={works.prew.desktopPic} />
+                  <img src={resultWork.prew.desktopPic} />
                   </div>
                 </div>
             </div>
             <div className={styles.reviewWrapper}>
               <div className={styles.reviewWrapper__pic}>
-                <img src={works.testimonial.authorPic} />
+                <img src={resultWork.testimonial.authorPic} />
               </div>
               <div className={styles.reviewWrapper__text}>
                 <blockquote>
                   <span className={styles.reviewWrapper__q}></span>
-                  <p>{works.testimonial.text}</p>
+                  <p>{resultWork.testimonial.text}</p>
                 </blockquote>
-                <div className={styles.reviewWrapper__author}>{works.testimonial.author}</div>
+                <div className={styles.reviewWrapper__author}>{resultWork.testimonial.author}</div>
               </div>
               <div className={styles.reviewWrapper__video}>
               <Video
-                  src={works.testimonial.video.src} 
-                  type={works.testimonial.video.type} 
-                  poster={works.testimonial.video.poster} 
+                  src={resultWork.testimonial.video.src} 
+                  type={resultWork.testimonial.video.type} 
+                  poster={resultWork.testimonial.video.poster} 
                 />
               </div>
             </div>
             <SocialMediaDark/>
-            <Pagination/>
+            <Pagination prevW = {prevWork} nextW = {nextWork}/>
         </div>  
           
           
